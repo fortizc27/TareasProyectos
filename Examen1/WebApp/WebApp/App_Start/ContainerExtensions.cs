d932dcede8b20adabbbf0c894075889582e1955b
fortiz <fortiz@gbm.net>
2021-10-22 21:08:38 -0600

    Creación de ventanas y botón eliminar.

--- Examen1/WebApp/WebApp/App_Start/ContainerExtensions.cs
+++ Examen1/WebApp/WebApp/App_Start/ContainerExtensions.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BD;
+using WBL;
 
 namespace WebApp
 {
@@ -11,7 +13,8 @@ namespace WebApp
 
         public static IServiceCollection AddDIContainer(this IServiceCollection services)
         {
-
+            services.AddSingleton<IDataAccess, DataAccess>();
+            services.AddTransient<IInquilinoService, InquilinoService>();
 
             return services;
         }
